8f6b5fa6b0f7a94f3c9f0117f5a055614cb60c39
RuchitaM12 <RuchitaM12>
2025-08-05 16:47:36 +0000

    changes done

--- use_case1/Raw_data.py
+++ use_case1/Raw_data.py
@@ -1,14 +1,29 @@
 # Databricks notebook source
-# MAGIC %sql
-# MAGIC USE catalog customer_catalog;
+# MAGIC %md
+# MAGIC ## Using the catalog created for this use case
+
+# COMMAND ----------
+
+# MAGIC %run /Workspace/Users/mahatoruchita712@gmail.com/PySpark-use-cases/use_case1/utils
 # MAGIC
 
 # COMMAND ----------
 
 
+"""
 df_bronze = spark.read.format("delta") \
                       .option("multiline", "True") \
                       .table("customer_catalog.bronze.orders")
+"""
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Reading table in bronze using read_file function
+
+# COMMAND ----------
+
+df_bronze = read_file("bronze", "orders", "json", "dropmalformed")
 
 # COMMAND ----------
 
@@ -16,4 +31,9 @@ display(df_bronze)
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC ## Writing to bronze layer using write_file function
 
+# COMMAND ----------
+
+df_bronze = write_file(df_bronze, "bronze", "orders", "json", "overwrite")
